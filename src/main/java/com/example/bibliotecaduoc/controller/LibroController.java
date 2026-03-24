54307eaf7525c77b338a869784c7ba79e47d06cf
YoungJose <jot.reyes@duocuc.cl>
2026-03-23 21:15:52 -0300

    finalizando

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -1,7 +1,7 @@
 package com.example.bibliotecaduoc.controller;
 
 import com.example.bibliotecaduoc.model.Libro;
-import com.example.bibliotecaduoc.service.LibroService;
+import com.example.bibliotecaduoc.services.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
